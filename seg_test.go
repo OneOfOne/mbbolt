acad061becaa9ca4712a677d72996265f7f8e807
Ahmed W <oneofone@gmail.com>
2023-04-13 13:34:08 -0500

    segdb/mdb: implement restore (#3)

--- seg_test.go
+++ seg_test.go
@@ -1,7 +1,9 @@
 package mbbolt
 
 import (
+	"path/filepath"
 	"strconv"
+	"sync"
 	"testing"
 )
 
@@ -24,4 +26,44 @@ func TestSegDB(t *testing.T) {
 			}
 		}
 	})
+}
+
+func TestSegBackupRestore(t *testing.T) {
+	tmp := t.TempDir()
+	sdb := NewSegDB(tmp, ".db", nil, 32)
+	defer sdb.Close()
+	var wg sync.WaitGroup
+	for i := 0; i < 10000; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			defer wg.Done()
+			sdb.Put("bucket", "key"+strconv.Itoa(i), i)
+		}()
+	}
+	wg.Wait()
+	zfp := filepath.Join(tmp, "segdb.zip")
+
+	if _, err := sdb.mdb.BackupToFile(zfp, nil); err != nil {
+		t.Fatal(err)
+	}
+	if err := sdb.Close(); err != nil {
+		t.Fatal(err)
+	}
+	sdb, err := NewSegDBFromFile(zfp, tmp, ".db", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if ln := len(sdb.dbs); ln != 32 {
+		t.Fatal("expected 100 dbs, got", ln)
+	}
+
+	for i := 0; i < 10000; i++ {
+		var v int
+		sdb.Get("bucket", "key"+strconv.Itoa(i), &v)
+		if v != i {
+			t.Fatalf("expected %d, got %d", i, v)
+		}
+	}
 }
